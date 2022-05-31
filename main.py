6a2041390006298db70d5e691c1129892be182fa
petmetic <101994210+petmetic@users.noreply.github.com>
2022-05-31 19:36:26 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect
+from sqla_wrapper import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String, unique=False)
+    text = db.Column(db.String, unique=False)
+
+
+db.create_all()
+
+
+@app.route("/")
+def index():
+    messages = db.query(Message).all()
+
+    print(messages)
+    return render_template("index.html", messages=messages)
+
+
+@app.route("/add-message", methods=["POST"])
+def add_message():
+    username = request.form.get("username")
+    text = request.form.get("text")
+
+    message = Message(author=username, text=text)
+    message.save()
+
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run()
